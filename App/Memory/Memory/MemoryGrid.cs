380affe90e1e029ad8274bb00fd4ef8b4cb4c163
Joeri de Wagt <joeridewagt@gmail.com>
2018-10-28 20:46:08 +0100

    Added the randominze grid with images

--- App/Memory/Memory/MemoryGrid.cs
+++ App/Memory/Memory/MemoryGrid.cs
@@ -5,18 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Memory
 {
     class MemoryGrid
     {
         private Grid grid;
+        private int rows, columns;
 
         public MemoryGrid(Grid grid, int colums, int rows)
         {
             this.grid = grid;
-            initGrid(colums, rows);
+            this.rows = rows;
+            this.columns = colums;
             AddLabel();
+            AddImages();
+            
+            initGrid(colums, rows);
+            
         }
         private void initGrid(int colums, int rows)
         {
@@ -29,6 +37,45 @@ namespace Memory
                 grid.ColumnDefinitions.Add(new ColumnDefinition());
             }
         }
+        private void AddImages()
+        {
+            List<ImageSource> images = GetImagesList();
+            int counter = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for(int column = 0; column < columns; column++)
+                {
+                    Image backgroundImage = new Image();
+                    backgroundImage.Source = images[counter++];
+                    Grid.SetColumn(backgroundImage, column);
+                    Grid.SetRow(backgroundImage, row);
+                    grid.Children.Add(backgroundImage);
+                }
+            }
+        }
+        private List<ImageSource> GetImagesList()
+        {
+            List<ImageSource> images = new List<ImageSource>();
+            for(int i = 0; i < 16; i++)
+            {
+                int imageNr = i % 8 + 1;
+                ImageSource source = new BitmapImage(new Uri("Images/"+imageNr+".png", UriKind.Relative));
+                images.Add(source);
+            }
+            Random random = new Random();
+            int n = images.Count;
+            for (int i = 0; i < images.Count; i++)
+            {
+                
+                int rnd = random.Next(i + 1);
+
+                ImageSource value = images[rnd];
+                images[rnd] = images[i];
+                images[i] = value;
+            }
+            return images;
+        }
+
         private void AddLabel()
         {
             Label title = new Label();
