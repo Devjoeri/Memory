6815f2e09acd8c14af86960c1dafb2ef4bb54623
iAzDev <az_dev@null.noreply.github.org>
2018-11-02 13:17:16 +0100

    settings page

--- App/Memory/Memory/MainMenu.xaml.cs
+++ App/Memory/Memory/MainMenu.xaml.cs
@@ -64,5 +64,15 @@ namespace Memory
         {
 
         }
+
+        /// <summary>
+        /// Go to settings page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void to_Settings(object sender, RoutedEventArgs e)
+        {
+            _navigator.Navigate(new settings(_navigator));
+        }
     }
 }
---
+++ App/Memory/Memory/settings.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Hier kun je een ander thema kiezen
+    /// Met behulp van een dropdown menu
+    /// </summary>
+    public partial class settings : Page
+    {
+        private INavigator _navigator;
+        public settings(INavigator navigator)
+        {
+            _navigator = navigator;
+            InitializeComponent();
+            add_Options();
+        }
+
+        /// <summary>
+        /// Add all folders to buttons, custom themes
+        /// </summary>
+        private void add_Options()
+        {
+            string[] folderArray = Directory.GetDirectories(".../.../testSettings");
+            foreach(string folder in folderArray)
+            {
+                Button btn = new Button();
+                btn.Width = 500;
+                btn.Height = 25;
+                btn.Content = folder;
+                settings_Stackpanel.Children.Add(btn);
+            }
+        }
+    }
+}
